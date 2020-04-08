bb062d61434d2bd552653119079e916ba4c065d6
Gavin <gang.dai@hand-china.com>
2020-04-08 11:08:56 +0800

    [ADD] ADD a top menu, user and manager group for library application

--- custom-addons/library_app/__manifest__.py
+++ custom-addons/library_app/__manifest__.py
@@ -3,5 +3,11 @@
     'description': 'Manage library book catalogue and lending.',
     'author': 'Gavin',
     'depends': ['base'],
-    'application': True
+    'application': True,
+    'data':[
+        # 注意library_security.xml 加在library_menu.xml文件之前，数据文件的加载顺序非常重要，因为我们只能引用已经定义过的标识符。
+        # 菜单项经常引用到安全组，所以建议将安全组定义文件放到菜单和视图文件之前。
+        'security/library_security.xml',
+        'views/library_menu.xml',
+    ]
 }

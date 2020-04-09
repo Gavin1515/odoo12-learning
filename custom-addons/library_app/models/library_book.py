ebe8be492f08ba7a270874b97a569c8fa6708b70
Gavin <1515984717@qq.com>
2020-04-09 21:24:44 +0800

    [ADD] Add check isbn business logic for library,book model

--- custom-addons/library_app/models/library_book.py
+++ custom-addons/library_app/models/library_book.py
@@ -3,7 +3,8 @@
 
 # Author: Gavin
 
-from odoo import fields, models
+from odoo import fields, models, api
+from odoo.exceptions import Warning
 
 class Book(models.Model):
     _name = 'library.book' # 仅有模型名使用点号(.) 来分割关键字，其它如模块、XML 标识符、数据表名等都使用下划线(_)
@@ -16,3 +17,24 @@ class Book(models.Model):
     image = fields.Binary(string='Cover')
     publisher_id = fields.Many2one(comodel_name='res.partner', string='Publisher')
     author_ids = fields.Many2many(comodel_name='res.partner', string="Authors")
+
+    @api.multi
+    def _check_isbn(self):
+        self.ensure_one()
+        isbn = self.isbn.replace('-', '')
+        digits = [int(x) for x in isbn if x.isdigit()]
+        if len(digits) == 13:
+            ponderations = [1, 3] * 6
+            terms = [a * b for a, b in zip(digits[:12], ponderations)]
+            remain = sum(terms) % 10
+            check = 10 - remain if remain != 0 else 0
+            return digits[-1] == check
+
+    @api.multi
+    def action_check_isbn(self):
+        for book in self:
+            if not book.isbn:
+                raise Warning("Please provide an ISBN for %s" % book.name)
+            if book.isbn and not book._check_isbn():
+                raise Warning('%s is an invalid ISBN' % book.isbn)
+            return True

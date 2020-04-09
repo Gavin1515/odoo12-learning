dbdd90c07340d10d8ec037d9ca458cb31809c255
Gavin <1515984717@qq.com>
2020-04-09 06:32:39 +0000

    [ADD] Add library.book model

--- custom-addons/library_app/__init__.py
+++ custom-addons/library_app/__init__.py
@@ -2,3 +2,5 @@
 # -*- coding: utf-8 -*-
 
 # Author: Gavin
+
+from . import models
---
+++ custom-addons/library_app/models/library_book.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Author: Gavin
+
+from odoo import fields, models
+
+class Book(models.Model):
+    _name = 'library.book' # 仅有模型名使用点号(.) 来分割关键字，其它如模块、XML 标识符、数据表名等都使用下划线(_)
+    _description = 'Book'
+
+    name = fields.Char(string='Title', required=True)
+    idbn = fields.Char(string='ISBN')
+    active = fields.Boolean(string='Active?', default=True)
+    date_published = fields.Date()
+    image = fields.Binary(string='Cover')
+    published_id = fields.Many2one(comodel_name='res.partner', string='Publisher')
+    author_ids = fields.Many2many(comodel_name='res.partner', string="Authors")

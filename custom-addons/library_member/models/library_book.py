b08e3688971e7148bcc5fa4112631b07dbdd1f78
Gavin <1515984717@qq.com>
2020-04-12 23:14:52 +0800

    [ADD] Add the inherit -f _check_isbn method in library.book

--- custom-addons/library_member/models/library_book.py
+++ custom-addons/library_member/models/library_book.py
@@ -3,7 +3,7 @@
 
 # Author: Gavin
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 class Book(models.Model):
     # _name是模型标识符，如果修改会发生什么呢？其实你可以修改，这时它会创建所继承模型的拷贝，成为一个新模型。这叫作原型继承
@@ -15,3 +15,18 @@ class Book(models.Model):
     isbn = fields.Char(help='Use a valid ISBN-13 or ISBN-10.')
     # 继承publisher_id字段，添加数据库索引属性，为该字段添加索引使查询更快，其它属性自动继承
     publisher_id = fields.Many2one(index=True)
+
+
+    # 继承_check_isbn方法，增加10位isbn的校验逻辑
+    @api.multi
+    def _check_isbn(self):
+        self.ensure_one()
+        isbn = self.isbn.replace('-', '')
+        digits = [int(x) for x in isbn if x.isdigit()]
+        if len(digits) == 10:
+            ponderators = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+            total = sum(a * b for a, b in zip(digits[:9], ponderators))
+            check = total % 11
+            return digits[-1] == check
+        else:
+            return super()._check_isbn()

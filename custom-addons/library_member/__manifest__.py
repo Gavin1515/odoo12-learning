e848808d8e1c8944bdb1ba196ec33daeb067dbcb
Gavin <1515984717@qq.com>
2020-04-13 00:26:45 +0800

    [FIX] Fix the reference of the xml file

--- custom-addons/library_member/__manifest__.py
+++ custom-addons/library_member/__manifest__.py
@@ -16,6 +16,6 @@
         'views/book_view.xml',
         'views/library_menu.xml',
         'views/member_view.xml',
-        'views/book_list_template',
+        'views/book_list_template.xml',
     ]
 }

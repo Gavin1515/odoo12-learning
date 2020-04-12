3b9da18c8356198840dbe82affd8f7dfbdd22f0b
Gavin <1515984717@qq.com>
2020-04-13 00:25:32 +0800

    [ADD] Add QWeb inherit to display not available book

--- custom-addons/library_member/__manifest__.py
+++ custom-addons/library_member/__manifest__.py
@@ -16,5 +16,6 @@
         'views/book_view.xml',
         'views/library_menu.xml',
         'views/member_view.xml',
+        'views/book_list_template',
     ]
 }
--- custom-addons/library_member/controllers/main.py
+++ custom-addons/library_member/controllers/main.py
@@ -12,6 +12,7 @@ class BookExtended(Books):
         继承controller,在url中添加available参数：library/books?available=1。
         如果不带参数，将会保留父类中定义的路由。
         但也可以为@http.route()装饰器添加参数，来重新定义或替换类路由。
+        例如：可以@http.route('/getbooks')来替换原来的/librayr/books路由，但是原/librayr/books就不可用了
     '''
     @http.route()
     def list(self, **kwargs):

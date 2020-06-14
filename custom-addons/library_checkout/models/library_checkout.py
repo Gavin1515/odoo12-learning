3a482cc06b317dc25dc938010aae82cade594d05
Gavin <1515984717@qq.com>
2020-06-14 17:11:33 +0800

    [ADD]  Chapter Tenth-Design User Interface: Add tree,form,search,calendar,pivot,graph,activity type views

--- custom-addons/library_checkout/models/library_checkout.py
+++ custom-addons/library_checkout/models/library_checkout.py
@@ -10,10 +10,18 @@ class Checkout(models.Model):
     _description = 'Checkout Request'
     _inherit = ['mail.thread','mail.activity.mixin']
 
-    member_id = fields.Many2one(comodel_name='library.member', required=True)
-    user_id = fields.Many2one(comodel_name='res.users', string='Librarion', default=lambda s: s.env.uid)
-    request_date = fields.Date(default=lambda s: fields.Date.today())
+    # track_visibility='onchange'表示带有该属性值的字段在被修改时记录到message中，但是track_visibility='onchange'不起作用。
+    member_id = fields.Many2one(comodel_name='library.member', required=True, track_visibility='onchange')
+    user_id = fields.Many2one(comodel_name='res.users', string='Librarion', default=lambda s: s.env.uid,
+                              track_visibility='onchange')
+    request_date = fields.Date(default=lambda s: fields.Date.today(), track_visibility='always')
     line_ids = fields.One2many(comodel_name='library.checkout.line',inverse_name='checkout_id', string='Borrowed Books')
+    num_other_checkouts = fields.Integer(compute='_compute_num_other_checkouts')
+
+    def _compute_num_other_checkouts(self):
+        for checkout in self:
+            domain = [('member_id','=',checkout.member_id.id),('state','in',['open']),('id','!=',checkout.id)]
+            checkout.num_other_checkouts = self.search_count(domain)
 
     @api.model
     def _default_stage(self):
@@ -34,11 +42,18 @@ class Checkout(models.Model):
     state = fields.Selection(related='stage_id.state')
     checkout_date = fields.Date(readonly=True)
     closed_date = fields.Date(readonly=True)
+    member_image = fields.Binary(related='member_id.partner_id.image')
+    num_books = fields.Integer(compute='_compute_num_books', store=True)
+
+    @api.depends('line_ids')
+    def _compute_num_books(self):
+        for checkout in self:
+            checkout.num_books = len(checkout.line_ids)
 
     @api.onchange('member_id')
     def onchange_member_id(self):
         today = fields.Date.today()
-        if self.request_date != today:
+        if self.request_date and self.request_date != today:
             self.request_date = today
             return {
                 'warning':{
@@ -50,6 +65,8 @@ class Checkout(models.Model):
             change方法还可以返回字段上的域限制字段取值范围：
             {‘user_id’: [(’email’, ‘!=’, False)]}
         '''
+    # 自odoo12开始，create()现在也可批量创建数据，这通过把单个字典对象修改为字典对象列表来传参进行实现。
+    # 这由带有@api.model_create_multi装饰器的create() 方法来进行支持。
     @api.model
     def create(self, vals):
         # Code before create: should use the `vals` dict
@@ -64,6 +81,26 @@ class Checkout(models.Model):
             raise exceptions.UserError('Not allowed to create a checkout in the done state.')
         return new_record
 
+    @api.multi
+    def write(self, vals):
+        for checkout in self:
+            # Code before write: can use `self`, with the old values
+            if 'stage_id' in vals:
+                new_state = self.env['library.checkout.stage'].browse(vals['stage_id']).state
+                if new_state == 'open' and checkout.state != 'open':
+                    vals['checkout_date'] = fields.Date.today()
+                if new_state == 'done' and checkout.state != 'done':
+                    vals['closed_date'] = fields.Date.today()
+        result = super().write(vals)
+        # Code after write: can use `self`, with the updated values
+        return result
+
+    @api.multi
+    def button_done(self):
+        done_stage = self.env['library.checkout.stage'].search([('state','=','done')], limit=1)
+        for checkout in self:
+            checkout.stage_id = done_stage
+        return True
 
 class CheckoutLine(models.Model):
     _name = 'library.checkout.line'
